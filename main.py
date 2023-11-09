a50911374b2dc7d482ab085edb0dcff1cc38a733
unknown <diasshag@gmail.com>
2023-11-09 13:18:19 +0600

    ve1

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 import psycopg2
+
+conn= psycopg2.connect(host = 'localhost',
+                        database = 'practice2',
+                        user = 'postgres',
+                        password = '123')
+cur = conn.cursor()
+cur.execute('SELECT superhero.name FROM superhero.superhero LIMIT 5;')
+usernames = [r[0] for r in cur.fetchall()]
+found = False
+while not Found:
+    username = input('введите ваш логин:')
+    if username in usernames:
+        print('вы есть в списке')
+        Found=True
+else:
+        print('мы не смогли вас найти')
+      
+conn.commit()
+cur.close()
+conn.close()
