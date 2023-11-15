03c8d4826a185ac829fba85f18b7951e66a4f960
Stem Stem <stem@astanait.edu.kz>
2023-11-15 09:38:28 +0600

    ver 1.6

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import psycopg2
+import csv
 
 conn= psycopg2.connect(host = 'localhost',
-                        database = 'assignments',
-                        user = 'postgres',
+                        database = 'postgres2',
+                        user = 'postgres'
+                        password = '1234',
                         )
 cur = conn.cursor()
 cur.execute('SELECT superhero.name FROM superhero.superhero LIMIT 5;')
+
+with open('csv2.csv', 'w', newline='', encoding='utf-8') as csvfile:  
+    csvwriter = csv.writer(csvfile)  
+    csvwriter.writerows(results)  
+  
+  
+  
+cur.close()  
+conn.close()
+
 usernames = [r[0] for r in cur.fetchall()]
 # found = False
 # while not Found:
